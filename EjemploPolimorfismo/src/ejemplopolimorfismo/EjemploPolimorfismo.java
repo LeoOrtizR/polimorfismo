24fabf10a6c0e4fe051ec3ef45c8664f191c979c
Leonardo Ortiz Ramirez <lortizr@uniremingtonmanizales.edu.co>
2020-10-02 23:35:14 -0500

    Se instancio la clase Vehiculo y se le dio el comportamiento de las Subclases VehiculoDeportivo, VehiculoTurismo

--- EjemploPolimorfismo/src/ejemplopolimorfismo/EjemploPolimorfismo.java
+++ EjemploPolimorfismo/src/ejemplopolimorfismo/EjemploPolimorfismo.java
@@ -5,6 +5,16 @@ public class EjemploPolimorfismo {
    
     public static void main(String[] args) {
         
+        Vehiculo vehiculo[] = new Vehiculo[3]; 
+        
+        vehiculo[0] = new Vehiculo("DQF99D","AKT","2015");
+        vehiculo[1] = new VehiculoDeportivo("DQF99D","AKT","2015",4);
+        vehiculo[2] = new VehiculoTurismo("DQF99D","AKT","2015",125);
+        
+        for(Vehiculo v : vehiculo){
+            v.mostrarDatos();
+            System.out.println("");
+        }
     }
     
 }
